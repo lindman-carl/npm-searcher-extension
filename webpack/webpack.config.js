98a6c616124d90e3da7d4e8a1a5d3223cb03807c
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-18 14:00:27 +0200

    fixed .env not working

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const DotenvWebpackPlugin = require("dotenv-webpack");
+const webpack = require("webpack");
 module.exports = {
   mode: "production",
   entry: {
@@ -27,5 +28,8 @@ module.exports = {
       patterns: [{ from: ".", to: ".", context: "public" }],
     }),
     new DotenvWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
   ],
 };
